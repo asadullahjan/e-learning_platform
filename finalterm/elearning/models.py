cb67431bffbe7ad7770f7f6c885c3165c497e79d
Asad <asadullah.jan@outlook.com>
2025-08-07 20:07:23 +0500

    feat: add course model

--- finalterm/elearning/models.py
+++ finalterm/elearning/models.py
@@ -15,7 +15,7 @@ class User(AbstractUser):
 
     # Override email field to make it unique
     email = models.EmailField(unique=True)
-    
+
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     profile_picture = models.ImageField(
         upload_to="profile_pics/", null=True, blank=True
@@ -29,3 +29,25 @@ class User(AbstractUser):
 
     class Meta:
         db_table = "users"
+
+
+class Course(models.Model):
+    """
+    Model for courses.
+    """
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    teacher = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="courses_taught"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "courses"
+        ordering = ["-created_at"]
