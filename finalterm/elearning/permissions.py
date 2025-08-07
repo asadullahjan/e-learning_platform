c586d00e0d402ce4e5c34cd889308caaeda561dd
Asad <asadullah.jan@outlook.com>
2025-08-07 20:07:45 +0500

    feat: add IsCourseOwner permission

--- finalterm/elearning/permissions.py
+++ finalterm/elearning/permissions.py
@@ -9,3 +9,9 @@ class IsTeacher(BasePermission):
 class IsStudent(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == "student"
+
+
+class IsCourseOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Only the course creator can edit/delete
+        return obj.teacher == request.user
